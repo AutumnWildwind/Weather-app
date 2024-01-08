07d0c7728eec49c49d62eca01f6f275eeeaec3be
DanielaC <142808667+AutumnWildwind@users.noreply.github.com>
2024-01-08 19:31:49 +0000

    Injected forecast HTML from JS

--- src/index.js
+++ src/index.js
@@ -58,3 +58,28 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Dublin");
+
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+      <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max">
+              <strong>18° </strong>
+            </span>
+              <span class="weather-forecast-temperature-min"> 12° </span>
+          </div>
+        </div>
+        `;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+displayForecast();
